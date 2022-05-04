336c54ec7f88e9bf4a533dfe245bff33ac937d0b
roshan911 <380534931@qq.com>
2022-05-04 23:58:13 +0800

    add files

---
+++ story/pages/normal/button.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>button</div>
+</template>
+
+<script>
+export default {
+  name: "rs-button",
+};
+</script>
+
+<style></style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const resolve = (filePath) => path.resolve(__dirname, filePath);
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "docs",
   publicPath: "/roshan-ui-vue/",
+  pages: {
+    index: {
+      entry: resolve("story/main.ts"),
+      template: "public/index.html",
+      filename: "index.html",
+      title: "roshan-ui-vue",
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugins.delete("prefetch-index").delete("preload-index");
+    config.resolve.alias.set("story", resolve("story"));
+  },
 });
