2faf4761f92b01f80181e6e38b64274337a7a53a
roshan911 <380534931@qq.com>
2022-05-06 00:18:39 +0800

    app-navigator

--- story/components/navigator/app-navigator.tsx
+++ story/components/navigator/app-navigator.tsx
@@ -1,8 +1,57 @@
-import { defineComponent } from "vue";
+import { defineComponent, inject, onBeforeUnmount, provide, reactive } from "vue";
+
+interface Route {
+  path?: string;
+  hash?: string;
+  params?: { [key: string]: string };
+}
+function getRoute(): Route {
+  let locationHash = window.location.hash || "";
+  if (locationHash.charAt(0) === "#") {
+    locationHash = locationHash.slice(1);
+  }
+  const [path, hash] = locationHash.split("#");
+  return { path, hash };
+}
+
+const APP_NAVIGATOR_PROVIDER = "@@app-navigator";
+
+function useAppNavigator(props: { defaultPath?: string }) {
+  const currentRoute = getRoute();
+  !currentRoute.path && (currentRoute.path = props.defaultPath);
+
+  const state = reactive({ route: currentRoute });
+  const methods = {
+    go(path: string) {
+      window.location.hash = encodeURIComponent(path);
+    },
+  };
+  const handler = {
+    hashchange: () => {
+      state.route = getRoute();
+    },
+  };
+  window.addEventListener("hashchange", handler.hashchange);
+  onBeforeUnmount(() => window.removeEventListener("hashchange", handler.hashchange));
+  const refer = {
+    state,
+    methods,
+  };
+  provide(APP_NAVIGATOR_PROVIDER, refer);
+  return refer;
+}
+
+export function injectAppNavigator() {
+  return inject(APP_NAVIGATOR_PROVIDER) as ReturnType<typeof useAppNavigator>;
+}
 
 export const AppNavigator = defineComponent({
-  name: 'app-navigator',
-  setup(props) {
-    return () => <div>Navss</div>
-  }
-})
+  name: "app-navigator",
+  props: {
+    defaultPath: String,
+  },
+  setup(props, context) {
+    useAppNavigator(props);
+    return () => (!!context.slots.default ? context.slots.default() : null);
+  },
+});
