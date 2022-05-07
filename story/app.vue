9c287b96ef2934f83a1a2dd0cda3c7dc5c4b2ea6
yu_dai <yu_dai@sonix.com.cn>
2022-05-07 18:07:25 +0800

    update

--- story/app.vue
+++ story/app.vue
@@ -1,20 +1,55 @@
 <template>
-  <div>hello word</div>
-  <AppNavigator defaultPath="normal/button">
-    <app-navigator-page />
-  </AppNavigator>
+  <app-navigator defaultPath="normal/button">
+    <article class="app-home">
+      <section class="app-header" :style="{height: HomeConfig.headSize + 'px'}">roshan-ui-vue</section>
+      <section class="app-menu">app-menu</section>
+      <section class="app-content">
+        app-content
+        <app-navigator-page />
+      </section>
+    </article>
+  </app-navigator>
 </template>
 
-<script>
-import { AppNavigator } from "./components/navigator/app-navigator";
-import { AppNavigatorPage } from "./components/navigator/app-navigator-page";
-export default {
-  name: "app",
-  components: {
-    AppNavigator,
-    AppNavigatorPage,
-  },
-};
+<script setup lang="ts">
+import { ref } from 'vue';
+import { AppNavigator } from './components/navigator/app-navigator';
+import { AppNavigatorPage } from './components/navigator/app-navigator-page';
+const color = ref('green')
+const HomeConfig = {
+  headSize: 60,
+  menuSize: 300
+}
+
+
 </script>
 
-<style></style>
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+.app-home{
+  .app-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    box-sizing: border-box;
+    background: v-bind(color);
+  }
+  .app-menu {
+    position: fixed;
+  }
+  .app-content {
+    min-height: 100vh;
+    box-sizing: border-box;
+  }
+}
+</style>
--- story/components/navigator/app-navigator.tsx
+++ story/components/navigator/app-navigator.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, inject, onBeforeUnmount, provide, reactive } from "vue";
+import { defineComponent, inject, onBeforeUnmount, provide, reactive } from 'vue';
 
 interface Route {
   path?: string;
@@ -6,19 +6,20 @@ interface Route {
   params?: { [key: string]: string };
 }
 function getRoute(): Route {
-  let locationHash = window.location.hash || "";
-  if (locationHash.charAt(0) === "#") {
+  let locationHash = window.location.hash || '';
+  if (locationHash.charAt(0) === '#') {
     locationHash = locationHash.slice(1);
   }
-  const [path, hash] = locationHash.split("#");
+  const [path, hash] = decodeURIComponent(locationHash).split('#');
   return { path, hash };
 }
 
-const APP_NAVIGATOR_PROVIDER = "@@app-navigator";
+const APP_NAVIGATOR_PROVIDER = '@@app-navigator';
 
 function useAppNavigator(props: { defaultPath?: string }) {
   const currentRoute = getRoute();
   !currentRoute.path && (currentRoute.path = props.defaultPath);
+  window.location.hash = encodeURIComponent(currentRoute.path!);
 
   const state = reactive({ route: currentRoute });
   const methods = {
@@ -27,12 +28,12 @@ function useAppNavigator(props: { defaultPath?: string }) {
     },
   };
   const handler = {
-    hashchange: () => {
+    hashchange: () => { 
       state.route = getRoute();
     },
   };
-  window.addEventListener("hashchange", handler.hashchange);
-  onBeforeUnmount(() => window.removeEventListener("hashchange", handler.hashchange));
+  window.addEventListener('hashchange', handler.hashchange);
+  onBeforeUnmount(() => window.removeEventListener('hashchange', handler.hashchange));
   const refer = {
     state,
     methods,
@@ -46,7 +47,7 @@ export function injectAppNavigator() {
 }
 
 export const AppNavigator = defineComponent({
-  name: "app-navigator",
+  name: 'app-navigator',
   props: {
     defaultPath: String,
   },
