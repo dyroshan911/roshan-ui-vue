1749997113010d86681e7afd3fcd321c200ad338
roshan911 <380534931@qq.com>
2022-05-14 00:36:04 +0800

    input clear

--- src/components/input/inputs.tsx
+++ src/components/input/inputs.tsx
@@ -10,9 +10,24 @@ export default defineComponent({
     const inputValue = ref('');
     const inputRef = ref<null | HTMLInputElement>(null);
     const classes = computed(() => ['rs-input', `rs-input-status-${props.status}`]);
+    const methods = {
+      clear() {
+        inputValue.value = '';
+      },
+      focus() {
+        inputRef.value!.focus();
+      },
+    };
+    const handleClear = () => {
+      inputValue.value = '';
+      inputRef.value!.focus();
+    };
     return () => (
       <div class={classes.value}>
-        <input class="rs-input-inner" type="text" v-model={inputValue.value} ref={inputRef} />
+        <input class='rs-input-inner' type='text' v-model={inputValue.value} ref={inputRef} />
+        <button class='rs-input-clear' onClick={methods.clear}>
+          clear
+        </button>
       </div>
     );
   },
--- story/pages/normal/input.vue
+++ story/pages/normal/input.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="demo-input">
-    <rs-input></rs-input>
+    <rs-input />
+    <h3>状态</h3>
+    <rs-input v-for="item in ['primary', 'success', 'warn', 'error', 'info']" :key="item" :status="item" style="margin-right: 8px; margin-top: 8px" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'demo-input'
-}
+  name: 'demo-input',
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
