e37f23c64b949088f95e419fb43573e1ecbda880
roshan911 <380534931@qq.com>
2022-06-30 23:44:08 +0800

    use Style

--- src/use/useStyle.ts
+++ src/use/useStyle.ts
@@ -1,3 +1,4 @@
+import { getCurrentInstance, inject, computed, provide } from 'vue';
 export enum StyleShape {
   fillet = 'fillet',
   round = 'round',
@@ -23,7 +24,47 @@ export const StyleProps = {
   size: { type: String },
   status: { type: String },
 };
+interface UseStyleProvideData {
+  shape: StyleShape;
+  size: StyleSize;
+  status?: StyleStatus;
+}
+
+interface UseStyleOption {
+  shape?: StyleShape;
+  size?: StyleSize;
+  status?: StyleStatus;
+  adjust?: (data: UseStyleProvideData) => void | UseStyleProvideData;
+}
 
 const USE_STYLE_PROVIDER = '@@USE_STYLE_PROVIDER';
 
-// export const useStyle =
+export const useStyle = (
+  option: UseStyleOption = {}
+): { styledComponent: { value: UseStyleProvideData } } => {
+  const ctx = getCurrentInstance() as any;
+  const parent = inject(USE_STYLE_PROVIDER, null) as null | {
+    value: UseStyleProvideData;
+  };
+  const defaultData = Object.assign(
+    { shape: StyleShape.fillet, size: StyleSize.normal },
+    option
+  );
+
+  const styleComputed = computed(() => {
+    const { shape, size, status } = ctx.props;
+    const parentData = !!parent ? parent.value : ({} as any);
+    let data: UseStyleProvideData = {
+      shape: shape || parentData.shape || defaultData.shape,
+      size: size || parentData.size || defaultData.size,
+      status: status || parentData.status || defaultData.status,
+    };
+    if (!!defaultData.adjust) {
+      data = defaultData.adjust(data) || data;
+    }
+    return data;
+  });
+
+  provide(USE_STYLE_PROVIDER, styleComputed);
+  return ctx;
+};
